57cfbba67c7e21026a0ba6650f6ddba6ded2daf9
Ali Gholami Monfared <Monfaredevops@gmail.com>
2025-07-05 21:05:23 +0330

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-# test.sh
 #!/bin/bash
 echo "[TEST] Running unit test..."
 EXPECTED="hello"
 ACTUAL=$(echo "hello")
+
 if [ "$EXPECTED" = "$ACTUAL" ]; then
   echo "[PASS] Test succeeded."
   exit 0
